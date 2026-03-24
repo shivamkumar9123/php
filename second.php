c4b561baeb415869a8da07a4be44159332c82c88
shivam kumar <shivamkumarmehta9123@gmail.com>
2026-03-25 01:08:07 +0530

    Add HTML form and PHP script in second.php

---
+++ second.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>MongoDB Form</h2>
+    <form method="POST" action="">
+        Name:<input type="text" name="name" require><br>
+        Email:<input type="email" name="email" required><br>
+        Password:<input type="password" name="pass"required><br>
+        <input type="submit" name="submit" value="submit">
+    </form>
+    <p>Hello World</p>
+    <?php
+    echo "Hello World";
+    echo "<br>";
+    $name="Shivam";
+     echo $name;
+     ?>
+</body>
+</html>
